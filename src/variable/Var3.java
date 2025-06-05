dee695a1373a197badc9881fefdcc9ce9f4230db
이영찬 <110439250+dhapdhap123@users.noreply.github.com>
2025-06-05 20:05:07 +0900

    변수 연습

---
+++ src/variable/Var3.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Var3 {
+    public static void main(String[] args) {
+        int a = 30;
+        System.out.println(30);
+
+        a = 50;
+        System.out.println(50);
+    }
+}
