f92eda446c9feaee94322cb33e7b5743e781d3e8
이영찬 <110439250+dhapdhap123@users.noreply.github.com>
2025-06-05 22:18:18 +0900

    문제 풀이 : 변수

---
+++ src/variable/ex/Question1.java
@@ -0,0 +1,12 @@
+package variable.ex;
+
+public class Question1 {
+    public static void main(String[] args) {
+        int num1 = 4, num2 = 3;
+        num1 = 5;
+        num2 = 7;
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+    }
+}
