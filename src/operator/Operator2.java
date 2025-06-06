56baaba794513ca74465353e34a7c7d473ee3cf4
이영찬 <110439250+dhapdhap123@users.noreply.github.com>
2025-06-06 12:02:51 +0900

    연산자

---
+++ src/operator/Operator2.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class Operator2 {
+    public static void main(String[] args) {
+        String a = "Hello ";
+        String b = "Java";
+        String c = a + b;
+        System.out.println(c);
+
+        // 문자열 + 숫자
+        String string = "a + b = " + 10;
+        System.out.println(string);
+    }
+}
