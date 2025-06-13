6c270763ce988e6ec3e0480ef3b362cde36a05fd
이영찬 <110439250+dhapdhap123@users.noreply.github.com>
2025-06-13 22:49:23 +0900

    패키지

---
+++ src/java_basic/pack/PackageMain2.java
@@ -0,0 +1,14 @@
+package java_basic.pack;
+
+//import java_basic.pack.a.User;
+//import java_basic.pack.a.User2;
+
+import java_basic.pack.a.*;
+
+public class PackageMain2 {
+
+    public static void main(String[] args) {
+        Data data = new Data();
+        User user = new User();
+    }
+}
