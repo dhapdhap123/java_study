70ffd450a773dfc3da8c6524c558df269525fb8c
이영찬 <110439250+dhapdhap123@users.noreply.github.com>
2025-06-16 22:53:47 +0900

    final

---
+++ src/java_basic/final1/Constant.java
@@ -0,0 +1,12 @@
+package java_basic.final1;
+
+public class Constant {
+
+    public static final double PI = 3.14;
+
+    public static final int HOURS_IN_DAY = 24;
+    public static final int MINUTES_IN_HOURS = 60;
+    public static final int SECONDS_IN_MINUTES = 60;
+
+    public static final int MAX_USERS = 1000;
+}
