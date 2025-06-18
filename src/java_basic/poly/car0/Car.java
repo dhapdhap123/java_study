b5e5a45cc64638b83763a61a6bf855ea39c9b9d3
이영찬 <110439250+dhapdhap123@users.noreply.github.com>
2025-06-18 23:23:20 +0900

    다형성과 설계

---
+++ src/java_basic/poly/car0/Car.java
@@ -0,0 +1,16 @@
+package java_basic.poly.car0;
+
+public class Car {
+
+    public void startEngine(){
+        System.out.println("K3Car.startEngine");
+    }
+
+    public void offEngine(){
+        System.out.println("K3Car.offEngine");
+    }
+
+    public void pressAccelerator(){
+        System.out.println("K3Car.pressAccelerator");
+    }
+}
