7f8778ec831b85862b62c2312432ece3cdd7607c
이영찬 <110439250+dhapdhap123@users.noreply.github.com>
2025-07-02 20:05:29 +0900

    예외 처리 2

---
+++ src/java_mid1/exception/ex4/exception/NetworkServiceV4.java
@@ -0,0 +1,19 @@
+package java_mid1.exception.ex4.exception;
+
+public class NetworkServiceV4 {
+
+    public void sendMessage(String data) {
+        String address = "https://example.com";
+        NetworkClientV4 client = new NetworkClientV4(address);
+        client.initError(data);
+
+        try {
+            client.connect();
+            client.send(data);
+        } finally {
+            client.disconnect();
+        }
+
+
+    }
+}
